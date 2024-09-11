ec46f477492980245e0b018bd501ef72484bdbf1
stanleyoffer <stanleyoffer@gmail.com>
2024-09-11 22:39:16 +0300

    Background

--- src/components/Navigation/NavigationBar.jsx
+++ src/components/Navigation/NavigationBar.jsx
@@ -8,8 +8,10 @@ const NavigationBar = () => {
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About us</a></li>
                 <li><a href="#services">Documents</a></li>
-                <li><a href="#portfolio">Portfolio</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="#portfolio">Calendar</a></li>
+                <li><a href="#contact">Galery</a></li>
+                <li><a href="#contact">Contacts</a></li>
+                <li><a href="#contact">News</a></li>
             </ul>
         </nav>
     );
