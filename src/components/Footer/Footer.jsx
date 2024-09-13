7bfdbacfe4144b53dbcc6c7f426a0749a9ced566
stanleyoffer <stanleyoffer@gmail.com>
2024-09-13 21:27:42 +0300

    Logo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,9 +5,18 @@ export default function Footer() {
     return (
         <div className="footer-wrapper">
             <div className='footer-copyright'><Text word="CopyRight"></Text></div>
-            <div className="footer-phone"><a href="tel:+359896865981">✆ <Text word="Phone"></Text></a></div>
-            <a href='mailto:firetkd@abv.bg' target='_blank'><strong><Text word="Email"></Text></strong></a>
-            <div className='footer-address'><Text word="Address"></Text></div>
+            <div className='footer-left'>
+                <div className='footer-text'>
+                    <div className="footer-phone"><a href="tel:+359896865981">✆ <Text word="Phone"></Text></a></div>
+                    <Text word="EmailText"></Text><a href='mailto:firetkd@abv.bg' target='_blank'><strong><Text word="Email"></Text></strong></a>
+                    <div className='footer-address'><Text word="Address"></Text></div>
+                </div>
+                <div className='footer-images'>
+                    <img src="/images/logo.png" alt="Descriptive Alt Text" className="footer-image-logo" />
+                    <img src="/images/firelogo.png" alt="Descriptive Alt Text" className="footer-image-firelogo" />
+                </div>
+            </div>
+
         </div>
     )
 }
