020656b724a94795dea1e788fa2d08b09c92302d
Nikola Nikolov <greeningsiren@gmail.com>
2024-09-14 18:34:34 +0300

    Added async to logout button!

--- src/components/Loginout/LogoutButton.jsx
+++ src/components/Loginout/LogoutButton.jsx
@@ -2,8 +2,8 @@ import supabase from '../../utils/supabase'
 import Text from './../Text/Text';
 
 export default function LogoutButton() {
-    const handleClick = () => {
-        supabase.auth.signOut()
+    const handleClick = async () => {
+        await supabase.auth.signOut()
         // localStorage.removeItem('sb-bqwrnxhhfnlqmgpskuzy-auth-token') // da e sigurno
         console.log("logout")
         // setTimeout(() => {window.location.href = '/'}, 500)
